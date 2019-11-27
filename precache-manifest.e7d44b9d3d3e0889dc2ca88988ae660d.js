d436d932a88620471eda9140cdfba2167161e0b7
Agri <Agriomarson@hotmail.com>
2019-11-27 16:06:14 +0100

    Updates

---
+++ precache-manifest.e7d44b9d3d3e0889dc2ca88988ae660d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbf229b29d77c75df39f528797b6e23",
+    "url": "/agri1995/FinderGithub.git/index.html"
+  },
+  {
+    "revision": "2a7d73ab74a820105d4a",
+    "url": "/agri1995/FinderGithub.git/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "c7b332a79d96a115e5f7",
+    "url": "/agri1995/FinderGithub.git/static/js/2.2a83fbfc.chunk.js"
+  },
+  {
+    "revision": "2a7d73ab74a820105d4a",
+    "url": "/agri1995/FinderGithub.git/static/js/main.c06730c0.chunk.js"
+  },
+  {
+    "revision": "48fa1bc6bd4b78ee709d",
+    "url": "/agri1995/FinderGithub.git/static/js/runtime~main.dd723e0f.js"
+  }
+]);
